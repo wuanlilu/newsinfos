0e4d02ce49081fc636772492a61422ce5d759d6d
wuanlilu <47544962+wuanlilu@users.noreply.github.com>
2019-03-14 15:48:07 +0800

    Add files via upload

--- manage.py
+++ manage.py
@@ -1,11 +1,28 @@
-from flask import Flask
-from config import dict_config
 
-app = Flask(__name__)
-app.config.from_object(dict_config['config'])
+from modules.admin.admin import admin_bule
+from modules.web.index import index_bule
+from modules.web.user import user_bule
+from modules import *
+from flask_script import Manager
+from flask_migrate import Migrate,MigrateCommand
+from flask_wtf import CSRFProtect
+from apps import app,db
+#注册后台管理员蓝图 url_prefix指定访问url不加时直接访问
+app.register_blueprint(admin_bule,url_prefix='/admin')
 
-@app.route('/')
-def index():
-    return "ok"
+#注册前台首页蓝图 url_prefix指定访问url不加时直接访问
+app.register_blueprint(index_bule,url_prefix='/index')
+
+app.register_blueprint(user_bule,url_prefix='/user')
+CSRFProtect(app)
+manage = Manager(app)
+migrate = Migrate(app,db)
+manage.add_command('db',MigrateCommand)
 if __name__ == '__main__':
     app.run()
+    # manage.run()
+
+
+
+
+
